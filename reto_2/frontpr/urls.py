51f7e86a3b623f630f020d3427b3414b2d1fc97f
Bryan Rodriguez <Serphp@hotmail.com>
2023-07-04 11:00:43 -0500

    update: routes

--- reto_2/frontpr/urls.py
+++ reto_2/frontpr/urls.py
@@ -3,7 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('mostrar_resultado/<int:puntaje_riesgo>/', views.mostrar_resultado, name='mostrar_resultado'),
-    #path('mostrar_resultado/', views.mostrar_resultado, name='mostrar_resultado'),
-    #path('mostrar_resultado/', views.descargar_resultado, name='descargar_resultado')
+    path('mostrar_resultado/<int:puntaje_riesgo>/<str:umbral_riesgo>/', views.mostrar_resultado, name='mostrar_resultado'),
 ]
